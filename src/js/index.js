0fa92d35d45fd6b2edf922d75499edb78e08e0e1
peibol888 <pabloalamovargas@gmail.com>
2021-10-02 09:31:28 +0000

    styles modification + photo

--- src/js/index.js
+++ src/js/index.js
@@ -33,7 +33,7 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
+          <h1>${variables.name} ${variables.lastname}</h1>
           <h2>Web Developer</h2>
           <h3>Miami, USA</h3>
           <ul class="position-right">
@@ -56,7 +56,7 @@ window.onload = function() {
     // this is the url of the image that will used as background for the profile cover
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL: "src/foto-cv.jpg",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
